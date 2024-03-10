ce33ed06ffc15066df718273cdda3e5ce24fca5e
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-10 16:45:11 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@
 import './App.css'
 
 function App() {
- 
+  
+
+  function handleClick(){
+    alert('button clicked')
+  }
+  
+  const handleClick2= () =>{
+    alert('button 2 cliced')
+  }
+  
+  const addFive = (num) => {
+    alert(num + 6)
+  }
+
 
   return (
     <>
      
       <h1>Vite + React</h1>
-   
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => {alert('third button clicked')}}>third button</button>
+      <button onClick={() => addFive(4)}>4 number</button>
     </>
   )
 }
