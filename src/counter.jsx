85fab6591e777e7fccdadd4dea45015b05e6d8d0
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-10 18:06:55 +0600

    added and reduce add

--- src/counter.jsx
+++ src/counter.jsx
@@ -7,10 +7,14 @@ const handleAdd = () =>{
   const newCount = count + 1;
   setCount(newCount)
 }
-
+const handleReduce = () =>{
+  const reduce = count - 1;
+  setCount(reduce)
+}
   return (
    <div style={{border: '2px solid yellow'}}>
     <h3 style={{fontSize: '5xl', bottom: '10px'}}>Counter : {count}</h3>
     <button  onClick={handleAdd} style={{bottom:'10px'}}>Add</button>
+    <button onClick={handleReduce}>Reduce</button>
    </div>
 )}
