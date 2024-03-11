7870dcbf95cfa7c740f3671904771588a7c52f57
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-11 15:19:00 +0600

    data collect

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./Box.css";
+import Friend from "./Friend";
 export default function Friends() {
   const [friends, setFriends] = useState([]);
 
@@ -12,6 +13,10 @@ export default function Friends() {
   return (
     <div>
       <h3>Friends : {friends.length}</h3>
+
+      {
+        friends.map(friend => <Friend friend={friends}></Friend>)
+      }
     </div>
   );
 }
